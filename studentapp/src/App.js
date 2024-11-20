f56628c0d0aeafedce2c4fb20cfd02e0ae0824aa
uovmadura <user1vavuniya@gmail.com>
2024-11-20 12:29:02 +0000

    students data is passed to child

--- studentapp/src/App.js
+++ studentapp/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import { students } from './data/StudentsDb';
+import StudentTable from './components/StudentTable';
 
 function App() {
   return (
@@ -10,7 +12,7 @@ function App() {
         <tbody>
           <tr>
             <td>
-              
+              <StudentTable students={students}/>
             </td>
           </tr>
         </tbody>
--- studentapp/src/components/StudentTable.js
+++ studentapp/src/components/StudentTable.js
@@ -1,7 +1,7 @@
 export default function StudentTable(props) {
     return(
         <div>
-
+            
         </div>
     )
 }

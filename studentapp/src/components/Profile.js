f2d14148afa8f1edb2793b6e85bff8aed93abb84
uovmadura <user1vavuniya@gmail.com>
2024-11-25 12:04:35 +0000

    Profile component added

---
+++ studentapp/src/components/Profile.js
@@ -0,0 +1,14 @@
+export default function Profile(props) {
+    return(
+        <>
+            <table>
+                <tr>
+                    <td rowSpan={7}>
+                        <img src={require('../assets/profilepic/1.jpg')} alt="profile picture" />
+                    </td>
+                    <td>ID </td>
+                </tr>
+            </table>
+        </>
+    )
+}

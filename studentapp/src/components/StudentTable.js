300d1101df3363a3ac43033d96175d1750144456
uovmadura <user1vavuniya@gmail.com>
2024-11-25 12:30:17 +0000

    Profile component called

--- studentapp/src/components/StudentTable.js
+++ studentapp/src/components/StudentTable.js
@@ -26,7 +26,7 @@ export default function StudentTable(props) {
                                     <td>{student.lastName}</td>
                                     <td>{student.course}</td>
                                     <td>{student.address.country}</td>
-                                    <td><button onClick={()=>{setStu(student)}}>View</button></td>
+                                    <td><button id={student.studentId} onClick={()=>{setStu(student)}}>View</button></td>
                                 </tr>
                             )
                         }
@@ -34,7 +34,7 @@ export default function StudentTable(props) {
                 </table>
             </div>
             <div className="rightDiv">
-                <Profile student={stu}/>
+                <Profile stu={stu}/>
             </div>
         </div>
     )
